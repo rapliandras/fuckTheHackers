411b35eb9e56ed530f7bc0eeb297ba18cc24835e
Rápli András <rapliandras@gmail.com>
2014-09-05 15:19:46 +0200

    Update example.php

--- example.php
+++ example.php
@@ -5,5 +5,9 @@
     throw new Exception("Original error message.");
     
 /* possible outcome: 
-You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'SELECT * FROM logins WHERE ' at line 1
+
+You have an error in your SQL syntax; check the manual that corresponds 
+to your MySQL server version for the right syntax to use near 'SELECT *
+FROM logins WHERE ' at line 1
+
 */
