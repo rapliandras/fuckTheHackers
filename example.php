e18494fe5c7685173519b369139ac75bf99eca55
Rápli András <rapliandras@gmail.com>
2014-09-05 15:18:29 +0200

    Update example.php

--- example.php
+++ example.php
@@ -3,3 +3,7 @@
 // Original error messages will be overridden. 
     set_exception_handler('fuckTheHackers');
     throw new Exception("Original error message.");
+    
+/* possible outcome: 
+You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'SELECT * FROM logins WHERE ' at line 1
+*/
